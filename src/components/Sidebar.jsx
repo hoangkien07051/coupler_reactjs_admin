d82cc8b4faa329813c45759775775c4dabc56e9b
hoang kien <yhoangkien07051@gmail.com>
2020-11-05 03:30:12 +0000

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,43 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Nav } from 'reactstrap';
 import PerfectScrollbar from 'react-perfect-scrollbar'
 
-const Sidebar = (props) => {
-                                  
-    const expandLogo = () => {
+export default class SideBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    expandLogo() {
         document.getElementById("logobg").classList.toggle("expand-logo");
     }
                     
-    const activeRoute = (routeName) => {
-        return props.location.pathname.indexOf(routeName) > -1 ? 'selected' : '';
+    activeRoute(routeName) {
+        return this.props.location.pathname.indexOf(routeName) > -1 ? 'selected' : '';
     }
 
-    return (
-        <aside className="left-sidebar" id="sidebarbg" data-sidebarbg="skin6" onMouseEnter={expandLogo.bind(null)} onMouseLeave={expandLogo.bind(null)}>
-            <div className="scroll-sidebar">
-                <PerfectScrollbar className="sidebar-nav">
-                    <Nav id="sidebarnav">
-                        {props.routes.map((prop, key) => {
-                            if (prop.redirect) {
-                                return null;
-                            }
-                            else {
-                                return (
-                                    // Adding Sidebar Item                                                           
-                                    <li className={activeRoute(prop.path) + (prop.pro ? ' active active-pro' : '') + ' sidebar-item'} key={key}>
-                                        <NavLink to={prop.path} className="sidebar-link" activeClassName="active">
-                                            <i className={prop.icon} />
-                                            <span className="hide-menu">{prop.name}</span>
-                                        </NavLink>
-                                    </li>
-                                );
-                            }
-                        })}
-                    </Nav>
-                </PerfectScrollbar>
-            </div>
-        </aside>
-    );
+    render() {
+        return (
+            <aside className="left-sidebar" id="sidebarbg" data-sidebarbg="skin6" onMouseEnter={this.expandLogo.bind(null)} onMouseLeave={this.expandLogo.bind(null)}>
+                <div className="scroll-sidebar">
+                    <PerfectScrollbar className="sidebar-nav">
+                        <Nav id="sidebarnav">
+                            {this.props.routes.map((prop, key) => {
+                                if (prop.redirect) {
+                                    return null;
+                                } else {
+                                    return (                                                      
+                                        <li className={this.activeRoute(prop.path) + (prop.pro ? ' active active-pro' : '') + ' sidebar-item'} key={key}>
+                                            <NavLink to={prop.path} className="sidebar-link" activeClassName="active">
+                                                <i className={prop.icon} />
+                                                <span className="hide-menu">{prop.name}</span>
+                                            </NavLink>
+                                        </li>
+                                    );
+                                }
+                            })}
+                        </Nav>
+                    </PerfectScrollbar>
+                </div>
+            </aside>
+        )
+    }
 }
-export default Sidebar;
+
+
+
